278f3fd425b3ad452729c58f699116d82e862b9c
Nimish <nimishsharma2612@gmail.com>
2024-06-08 23:05:35 +0530

    completed script.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,11 @@
+let editedPlayer = 0;
+
+const players = [{name:'',symbol:'X'}, {name:'',symbol:'O'}];
+
 const overlayElement = document.getElementById("config-overlay");
 const overlayBackdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
 const editPlayer1Button = document.getElementById("edit-player1-btn");
 const editPlayer2Button = document.getElementById("edit-player2-btn");
@@ -10,3 +16,4 @@ editPlayer2Button.addEventListener("click", openConfigOverlay);
 cancelConfigButton.addEventListener("click", closeConfigOverlay);
 overlayBackdropElement.addEventListener("click", closeConfigOverlay);
 
+formElement.addEventListener("submit", savePlayerConfig);
--- Scripts/config.js
+++ Scripts/config.js
@@ -1,4 +1,5 @@
-function openConfigOverlay() {
+function openConfigOverlay(event) {
+  editedPlayer = +event.target.dataset.playerid;
   overlayElement.style.display = "block";
   overlayBackdropElement.style.display = "block";
 }
@@ -6,4 +7,28 @@ function openConfigOverlay() {
 function closeConfigOverlay() {
   overlayElement.style.display = "none";
   overlayBackdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent='';
+  formElement.firstElementChild.lastElementChild.value = '';
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault(); //prevent form default behaviour page wont submit
+  const formData = new FormData(event.target); //Instantiating an object based on the object blueprint
+  //formData will automatically pick up the input fields from the event
+  const enteredPlayerName = formData.get("playername").trim(); //Get the value of one of our inputs by entering the id of the element we want to extract
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById('player'+editedPlayer+'-data');
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer-1].name=enteredPlayerName;
+
+  closeConfigOverlay();
+
 }
